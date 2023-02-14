3d5c7b88d8862abde3be4431f13e387d852b72d7
Det Yang <detyang22@gmail.com>
2023-02-14 17:21:51 -0600

    Add files via upload
    draft archive - standard mode

--- server.R
+++ server.R
@@ -1,9 +1,86 @@
+####SERVER.R module----
+
+###Load packages----
+library(DT)
 library(gridExtra)
+library(plotly)
 library(scales)
 library(shiny)
-library(shinythemes)
+library(shinydashboard)
+library(shinydashboardPlus)
 library(shinyjs)
+library(shinythemes)
+library(shinyWidgets)
 
+
+### Functions----
+## Preprocess player data table
+fun_prep_drf <- function(df){
+  df <- df[, c(1, 3, 4, 5, 6, 14, 15)]
+  df <- df[, c(7, 1 : 6)]
+  colnames(df) <- c("Headshot", "Year", "Round", "Pick", "Name", "From", "Team_abb")
+  df <- df %>%
+    mutate(
+      Headshot = paste0("<img src=\"", Headshot, "\" width=\"60\"</img>")
+    ) %>%
+    mutate(
+      Team = paste0("<img src=\"RESOURCE/teams_logo/", Team_abb, ".png\" width=\"45\"</img>")
+    )
+  return(df)
+}
+## Get data by selected attributes
+fun_get_data_drf <- function(df, var_year, var_round, var_pick, var_team){
+  var_year <- ifelse(var_year == "All", 0, var_year)
+  var_round <- ifelse(var_round == "All", 0,
+                      ifelse(var_round == "First Round", 1, 2))
+  var_pick <- ifelse(var_pick == "All", 0, var_pick)
+  var_team <- ifelse(var_team == "All", 0, 
+                     df_team$slugTeam[which(df_team$nameTeam == var_team)])
+  if(var_year != 0){
+    df <- df[which(df$Year == var_year), ]
+  }
+  if(var_round != 0){
+    df <- df[which(df$Round == var_round), ]
+  }
+  if(var_pick != 0){
+    df <- df[which(df$Pick == var_pick), ]
+  }
+  if(var_team != 0){
+    df <- df[which(df$Team_abb == var_team), ]
+  }
+  df <- df[, -7]
+  return(df)
+}
+
+### Load data----
+## Read players and teams dataset and run preprocess for player dataset
+df_player <- read.csv("www/DATA/PLAYER_META.csv", 
+                      header = T, stringsAsFactors = F, check.names = F)
+df_team <- read.csv("www/DATA/TEAMS_META.csv", 
+                    header = T, stringsAsFactors = F, check.names = F)
+df_player_drf <- fun_prep_drf(df_player)
+
+
+### server module
 server <- function(input, output, session){
+  ## Reactive draft data table based on user selection
+  df_drf <- reactive({
+    fun_get_data_drf(df_player_drf, input$year_drf, input$round_drf, 
+                     input$pick_drf, input$team_drf)
+  })
   
+  ## Render table content in draft tab
+  output$tbl_drf <- DT::renderDataTable(
+    df_drf(), 
+    options = list(
+      initComplete = JS(
+        "function(settings, json) {",
+        "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff'});", "}"
+      ), 
+      scrollX = TRUE, pageLength = 30, 
+      lengthMenu = c(15, 30, 60), 
+      columnDefs = list(list(targets = '_all', className = 'dt-center'))
+    ), 
+    escape = FALSE, rownames = FALSE
+  )
 }
--- ui.R
+++ ui.R
@@ -1,16 +1,25 @@
+library(DT)
 library(gridExtra)
+library(plotly)
 library(scales)
 library(shiny)
-library(shinythemes)
+library(shinydashboard)
+library(shinydashboardPlus)
 library(shinyjs)
+library(shinythemes)
+library(shinyWidgets)
+
 
 df_key_date <- read.csv("www/DATA/KEY_DATE.csv", 
                         header = T, stringsAsFactors = F, check.names = F)
+df_player <- read.csv("www/DATA/PLAYER_META.csv",
+                      header = T, stringsAsFactors = F, check.names = F)
 var_gap <- max(as.numeric(as.Date(df_key_date$DATE[nrow(df_key_date)], 
                    format = "%m/%d/%Y") - Sys.Date()), 0)
 
-ui <- shiny::navbarPage(
-  # App Logo
+ui <- navbarPage(
+  theme = shinytheme("cosmo"), 
+  # App Logo & Title
   title = div(
     img(
       src = "RESOURCE/logo.png", 
@@ -19,12 +28,69 @@ ui <- shiny::navbarPage(
     )
   ), 
   windowTitle = "Draft Craft", 
-  theme = shinytheme("cosmo"), 
+  # Draft Class tab
+  tabPanel(
+    strong("Draft Archive"), 
+    fluidRow(
+      column(
+        9, 
+        box(
+          style = "background-color:#fff; border-color:#2c3e50; height:77vh", width = 12, 
+          DT::dataTableOutput("tbl_drf")
+        )
+      ), 
+      column(
+        3, 
+        box(
+          style = "background-color:#fff; border-color:#2c3e50; height:77vh", width = 12, 
+          radioGroupButtons(
+            inputId = "gbtn_drf", label = NULL, 
+            choices = c("Standard Mode", "Advanced Mode"), selected = "Standard Mode", 
+            justified = TRUE, size = "lg", 
+            checkIcon = list(
+              yes = icon("square-check"),
+              no = icon("square")
+            )
+          ), 
+          hr(), 
+          conditionalPanel(
+            "input.gbtn_drf === 'Standard Mode'", 
+            pickerInput(
+              inputId = "year_drf", label = h4(strong("Select draft year")), 
+              width = "100%", options = list(size = 5), 
+              choices = c("All", unique(df_player$yearDraft)), selected = "All"
+            ), 
+            pickerInput(
+              inputId = "round_drf", label = h4(strong("Select draft round")), 
+              width = "100%", 
+              choices = c("All", "First Round", "Second Round"), selected = "All"
+            ), 
+            pickerInput(
+              inputId = "pick_drf", label = h4(strong("Select draft pick")), 
+              width = "100%", options = list(size = 5), 
+              choices = c("All", 1 : 30), selected = "All"
+            ), 
+            pickerInput(
+              inputId = "team_drf", label = h4(strong("Select team")), 
+              width = "100%", options = list(size = 5), 
+              choices = c("All", sort(unique(df_player$nameTeam))), selected = "All"
+            )
+          )
+        )
+      )
+    )
+  ), 
+  tabPanel(
+    strong("Team Performance")
+  ), 
+  tabPanel(
+    strong("About")
+  ), 
   # Countdown module
   absolutePanel(
-    top = 0, right = 15, style = "z-index:9999; text-align: right;", 
+    top = 0, right = 35, style = "z-index:9999; text-align: right;", 
     p(
-      span(strong(var_gap), style = "color:#F9CC0B;font-size:220%"), 
+      span(strong(em(var_gap)), style = "color:#F9CC0B;font-size:220%"), 
       HTML('&nbsp;'), 
       span("Days to go before the ", 
            style = "color:white;font-size:150%;font-family:Verdana"), 
@@ -35,26 +101,19 @@ ui <- shiny::navbarPage(
            style = "color:white;font-size:150%;font-family:Verdana")
     )
   ), 
-  tabPanel(
-    strong("Draft Class")
-  ), 
-  tabPanel(
-    strong("Team Performance")
-  ), 
-  tabPanel(
-    strong("About")
-  ), 
   hr(style = "border-color: #cbcbcb;"), 
   fluidRow(
     column(
       11, 
-      p("Data used to generate this app are from nbastatR, obtain details from ", 
+      p("Data used in this app are from nbastatR, obtain more details from ", 
         tags$a(href = "https://rdrr.io/github/abresler/nbastatR/f/README.md", 
                'HERE', target = '_blank'), style = "font-size:85%;text-align:center"), 
-      p("App created by ", 
+      p("Created by ", 
         tags$a(href = "https://detyang.com", 'Det Yang', target = '_blank'), 
-        img(src = "RESOURCE/personal_logo.png", height = 64), 
-        "Find me on ", tags$a(href = "https://github.com/detyang", "Github", target = '_blank'), 
+        tags$a(href = "https://detyang.com", 
+               img(src = "RESOURCE/personal_logo.png", height = 42), target = "_blank"), 
+        "Find me on", 
+        tags$a(href = "https://github.com/detyang", "Github", target = "_blank"), 
         style = "font-size:85%;text-align:center"), 
       p("Have a question? Spot an error? Send an ",
         tags$a(href = "mailto:detyang22@gmail.com", "Email", target = '_blank'), 
